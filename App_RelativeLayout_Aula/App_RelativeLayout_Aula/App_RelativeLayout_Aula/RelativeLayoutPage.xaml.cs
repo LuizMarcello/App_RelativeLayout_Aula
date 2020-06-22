b09cd0be31020817d41d8452746706d4f672f0ea
luiz marcello <luizmarcello.vmo@hotmail.com>
2020-06-21 23:42:50 -0300

    21.06.2020_23:42

--- App_RelativeLayout_Aula/App_RelativeLayout_Aula/App_RelativeLayout_Aula/RelativeLayoutPage.xaml.cs
+++ App_RelativeLayout_Aula/App_RelativeLayout_Aula/App_RelativeLayout_Aula/RelativeLayoutPage.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +14,26 @@ namespace App_RelativeLayout_Aula
         public RelativeLayoutPage()
         {
             InitializeComponent();
+
+            var layout = new RelativeLayout();
+
+            var box1 = new BoxView { Color = Color.Lime };
+
+            var box2 = new BoxView { Color = Color.Purple };
+
+            layout.Children.Add(box1,
+
+                widthConstraint: Constraint.RelativeToParent(p => p.Width),
+
+                heightConstraint: Constraint.RelativeToParent(p => p.Height * .25));
+
+            layout.Children.Add(box2,
+
+                yConstraint: Constraint.RelativeToView(box1, (RelativeLayout, b1) => b1.Height + 50));
+
+            Content = layout;
+
+                
         }
     }
 }
